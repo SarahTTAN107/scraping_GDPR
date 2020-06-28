706b91d9a14aa1a25b1e2acb2c4a0f1fb2006a64
sarahTTTAN107 <sarah.tho.nguyen@gmail.com>
2020-06-28 16:06:50 +0300

    add pyppeteer, fixing some small errors in scraping

--- scraping.py
+++ scraping.py
@@ -1,27 +1,20 @@
 import asyncio
+import pyppeteer
 from pyppeteer import launch
 
 async def cookie_div_exist():
     browser = await launch({'headless': False})
     page = await browser.newPage()
     await page.goto('https://avaus.com/', { "waitUntil": "networkidle0" })
-    elements = await page.querySelector('div')
-    # sad approach 1 that definitely didn't work :( ---- section = await page.querySelector("//div[contains(., 'Cookies')]")
+    #elements = await page.querySelector('div')
+    #section = await page.querySelector("//div[contains(., 'Cookies')]")
     #section = await page.evaluate('(element) => element.innerHTML.indexOf("This website uses cookies" !== -1', element)
-
     #section = await page.evaluate('(elements) => elements[i].innerHTML.indexOf("This website uses cookies" !== -1', elements)
+
     paragraphs = await page.evaluate('//p[contains(text(), "cookies")]')
 
     #print(elements)
-
     print(paragraphs)
-
-    #if len(section) > 0:
-    #    print('FOUND IT!')
-    #else:
-    #    print('no cookie :(')
-
-    #await browser.close()
 
 
 asyncio.get_event_loop().run_until_complete(cookie_div_exist())
